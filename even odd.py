c7a604b3b2d7be3769b655497ffd209471e87296
hrshobha66-source <hrshobha66@gmail.com>
2026-08-28 23:11:11 +0530

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,17 @@
+print("Enter your number:")
+number = int(input())
+
+if number % 20 == 0:
+    print("Divisible by 20")
+else:
+    print("Not divisible by 20")
+
+
+
+
+number = int(input("Enter your number: "))
+
+if number % 2 != 0:
+    print("Odd")
+else:
+    print("Even")
