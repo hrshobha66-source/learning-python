bc7e915d7dc819600f7f56442c0fd86f51861341
hrshobha66-source <hrshobha66@gmail.com>
2026-08-30 23:11:54 +0530

    Add files via upload

---
+++ string_demo.py
@@ -0,0 +1,34 @@
+#superhero = input("Enter superhero name:")
+#print(superhero)
+#age = int(input("Enter your age:"))
+#print(age)
+#height =float(input("Enter your height:"))
+#print(height)
+#firstname = input("Enter frist name")
+#lastname = input("Enter last name:")    
+#print(firstname)
+#print(lastname)
+
+#string operatation
+#name = "shobha"
+#print(name.upper())
+
+#name = input("Enter your name:")
+#print("Helllo",name)
+
+#name=bhagya
+#print(name.lower())
+
+#find
+#name = "meenakshi"
+#print(name.find("n"))
+#print(name.find("ee"))
+
+#relpace
+name = "deepak"
+#print(name.replace("shobha","bhagya"))
+#print(name.replace("deepak","aaisha"))
+
+#check the presence
+#print("d"in name)
+print('t'in name)
