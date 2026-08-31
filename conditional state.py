046f2ee696c38d6db32a15250e5456e5d264005c
hrshobha66-source <hrshobha66@gmail.com>
2026-08-31 22:19:12 +0530

    Add files via upload

---
+++ conditional state.py
@@ -0,0 +1,20 @@
+#age = 19
+#if age>= 19:
+ #   print("you are adult")
+
+#age = 14
+#if age>= 19:
+#   print("you are adult")
+#else:
+#   print("your not adult")
+
+marks = 75
+if marks >= 80:
+    print("A")
+elif marks >=70:
+    print("B")
+else:
+    marks<=60
+    print("C")
+
+
