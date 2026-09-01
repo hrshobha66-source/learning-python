9317ebb1ce85a6be153689a7fa315bbc24d522c8
hrshobha66-source <hrshobha66@gmail.com>
2026-09-01 23:22:55 +0530

    Add files via upload

--- conditional state.py
+++ conditional state.py
@@ -1,20 +1,99 @@
 #age = 19
 #if age>= 19:
- #   print("you are adult")
+ # print("you are adult")
 
 #age = 14
 #if age>= 19:
-#   print("you are adult")
+ #  print("you are adult")
 #else:
-#   print("your not adult")
+ #   print("your not adult")
 
-marks = 75
-if marks >= 80:
-    print("A")
-elif marks >=70:
-    print("B")
-else:
-    marks<=60
-    print("C")
+#marks = 75
+#if marks >= 80:
+ #   print("A")
+#elif marks >=70:
+ #   print("B")
+#else:
+    #marks<=60
+    #print("C")
+
+#while loop
+#counter:1,2,3,4,5,6
+#
+#counter = 1
+#while counter <= 5:
+ #   print( "i am good girl")
+  #  counter +=1
+#print("end of code")
+
+#i=1
+#while i<=5:
+#   print(i*"*")
+#   i +=1
+
+#i=1
+#while i<=5:
+#   print(i*"amma")
+#   i+=1
+
+#for loop
+#for i in range(20):
+#   print(i)
+
+#for i in range(1, 5):
+#    print(i)
+
+#for i in range(1,10):
+#   if i%2==0:
+#       print(i)
+
+#for i in range (1,20):
+#   if i%5==0:
+#       print(i)
+
+#for i in range(1,50):
+#   if(i==21):
+#       continue
+#   if(i % 3 == 0):
+#       print(i) 
+#print("out of the loop")
+
+#for i in range(50,100):
+#   if (i == 80):
+#       break
+#   if(i % 7 == 0):
+#       print(i)
+
+#for i in range(1 ,20):
+#   if(i % 2 != 0):
+#       print(i)
+
+#for i in range (19,35):
+#   if(i % 3!= 0):
+#       print(i)
+
+#for i in range (1,10):
+#   print(i* 57)
+
+#for i in range (20,25):
+#   print(i*"57")
+
+#for i in range(1,50):
+#   if (i==15):
+#       break
+#   if( i % 3 == 0):
+#       print(i)
+
+a= int(input("Enter the a:"))
+b=int(input("Enter the b:"))
+for i in range(1,1000):
+    if (i % a ==0 ) and ( i % b == 0):
+        print(i)
+        break
+
+ 
+
+
+
 
 
