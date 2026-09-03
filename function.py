4da025e4de52aa08b2838127e7bc1418b254b898
hrshobha66-source <hrshobha66@gmail.com>
2026-09-03 23:15:33 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,16 @@
+# def sum(a,b):
+#     print(a+b)
+# sum(111, 222)
+
+#function define
+# def calc_gst(price):
+#     new_price = price + price * 0.18
+#     print(new_price)
+
+# #function call
+# calc_gst(100)
+# calc_gst(2050)
+
+print(len([1,2,3,4,5]) )
+    
+
